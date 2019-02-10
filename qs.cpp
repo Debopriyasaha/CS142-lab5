5ef880784a8a08a209ccf37f69a03ad38efbdf10
Debopriyasaha <42205516+Debopriyasaha@users.noreply.github.com>
2019-02-11 00:26:52 +0530

    Add files via upload

---
+++ qs.cpp
@@ -0,0 +1,60 @@
+//Today we will learn about Quicksort
+#include<iostream>
+using namespace std;
+
+//The quicksort function
+void qs(int a[],int f,int l)
+{
+	//When there is only 1 element present it will skip the entire if part and function will terminate
+	if(f<l)
+	{
+		int pivot=l,temp;
+		for(int i=l-1;i>=f;i--)
+		{
+			//If ith term bigger than pivot, store it in temp and shift the entire arrary upto pivot a space and put the ith term after pivot
+			if(a[i]>a[pivot])
+			{
+				temp=a[i];
+				for(int j=i;j<pivot;j++)
+				{
+					a[j]=a[j+1];
+				}
+				a[pivot]=temp;
+				//Update pivot
+				pivot--;
+			}
+		}
+		//Use recursion
+		qs(a,f,pivot-1);
+		qs(a,pivot+1,l);
+	}
+}
+
+//Function for displaying the array
+void disp(int a[],int n)
+{
+    cout << "Sorted array:-\n";
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	//declaring the variables
+	int n;
+	//taking the inputs
+	cout<<"What is the length of the array?\n";
+	cin>>n;
+	int a[n];
+	cout<<"now start entering elements"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	qs(a,0,n-1);
+	disp(a,n);
+return 0;
+}
